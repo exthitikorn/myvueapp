dd51fac2088d1849a355df62e17f6c8ba8e7afbf
exthitikorn <rockerzeeza@gmail.com>
2022-01-26 10:05:19 +0700

    Create userBackend

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,11 @@ const bodyParser = require('body-parser');
 global.Olt = require('./api/models/oltModel');
 global.Ofccc = require('./api/models/ofcccModel');
 global.Sdp = require('./api/models/sdpModel');
+global.User = require('./api/models/userModel');
 const oltroutes = require('./api/routes/oltRoutes');
 const ofcccroutes = require('./api/routes/ofcccRoutes');
 const sdproutes = require('./api/routes/sdpRoutes');
+const userroutes = require('./api/routes/userRoutes');
 
 mongoose.connect(
     'mongodb://localhost/myapp',
@@ -24,6 +26,8 @@ app.use(bodyParser.json())
 oltroutes(app);
 ofcccroutes(app);
 sdproutes(app);
+userroutes(app);
+
 app.listen(port);
 
 app.use((req, res)=>{
